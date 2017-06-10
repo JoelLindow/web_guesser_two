4cf8c248575e893166c60cf026447a8ca226b640
JoelLindow <joellindow@gmail.com>
2017-06-10 10:06:14 -0600

    Finished. Instructions toward the end were absolutely lacking. had to look at another completed version to complete. Stop making me guess and actually teach me how to do this please.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,55 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(0..100)
+SECRET_NUMBER = rand(0..100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"] || "x"
+  message = check_guess(guess)
+  erb :index, :locals => {
+                          :number => SECRET_NUMBER,
+                          :message => message,
+                          :color => determine_color(guess)
+                          }
+end
+
+# params["guess"]
+def check_guess(guess)
+  if guess == "x"
+    "Make a guess!"
+  elsif guess.to_i == SECRET_NUMBER
+    "You got it right\n The SECRET NUMBER is #{SECRET_NUMBER}"
+  elsif guess.to_i > SECRET_NUMBER
+    high_message(guess)
+  elsif guess.to_i < SECRET_NUMBER
+    low_message(guess)
+  end
+end
+
+def determine_color(guess)
+  if guess == "x"
+    "#ffffff"
+  elsif guess.to_i == SECRET_NUMBER
+   "#00cc66"
+ elsif (guess.to_i > (SECRET_NUMBER + 5)) || (guess.to_i < (SECRET_NUMBER - 5))
+   "#ff3333"
+ elsif (guess.to_i > SECRET_NUMBER) || (guess.to_i < SECRET_NUMBER)
+   "#ffcccc"
+ end
+end
+
+def high_message(guess)
+  if guess.to_i > (SECRET_NUMBER + 5)
+    "Way too high!"
+  else
+    "Too high!"
+  end
+end
+
+def low_message(guess)
+  if guess.to_i < (SECRET_NUMBER - 5)
+    "Way too low!"
+  else
+    "Too low"
+  end
 end
